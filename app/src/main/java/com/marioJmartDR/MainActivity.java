33c8229d91b6c20c8994f2e1231c48e71fda65bc
Mario Claudius <mcmario7901@gmail.com>
2021-11-30 23:16:50 +0700

    Tutam Modul 9

--- app/src/main/java/com/marioJmartDR/MainActivity.java
+++ app/src/main/java/com/marioJmartDR/MainActivity.java
@@ -1,22 +1,35 @@
 package com.marioJmartDR;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.material.tabs.TabLayout;
+import com.marioJmartDR.fragment.FilterFragment;
+import com.marioJmartDR.fragment.ProductFragment;
 import com.marioJmartDR.model.Account;
 
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tv = findViewById(R.id.textview_main_activity);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.view_pager);
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPageAdapter vpAdapter = new ViewPageAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new ProductFragment(), "PRODUCTS");
+        vpAdapter.addFragment(new FilterFragment(), "FILTER");
+        viewPager.setAdapter(vpAdapter);
         Account account = LoginActivity.getLoggedAccount();
-        tv.setText("Hello " + account.name);
     }
 }
---
+++ app/src/main/java/com/marioJmartDR/model/Product.java
@@ -0,0 +1,21 @@
+package com.marioJmartDR.model;
+
+public class Product extends Serializable{
+    public String name;
+    public int weight;
+    public boolean conditionUsed;
+    public double price;
+    public ProductCategory category;
+    public int accountId;
+    public double discount;
+    public byte shipmentPlans;
+
+    public String toString()
+    {
+        String str = "Name : " + this.name + "\nWeight : " + this.weight +
+                "\nconditionUsed : " + this.conditionUsed + "\nprice : " +
+                this.price + "\ncategory : " + this.category + "\ndiscount : " +
+                this.discount + "\naccountId : " + this.accountId;
+        return str;
+    }
+}
