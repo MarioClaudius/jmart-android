0b0cb1cde05de1e2c4cd4f28afe2d51c045f9352
Mario Claudius <mcmario7901@gmail.com>
2021-12-02 18:02:23 +0700

    Tutam Modul 9 (change page -> next/prev/go done)

--- app/src/main/java/com/marioJmartDR/fragment/ProductFragment.java
+++ app/src/main/java/com/marioJmartDR/fragment/ProductFragment.java
@@ -43,8 +43,11 @@ public class ProductFragment extends Fragment {
     String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
             "WebOS","Ubuntu","Windows7","Max OS X"};
 
+    private EditText edtPage;
+    private Button prevBtn, nextBtn, goBtn;
+    private ListView listView;
     List<Product> productList = new ArrayList<>();
-    List<String> productNameList = new ArrayList<>();
+//    List<String> productNameList = new ArrayList<>();
     private static final Gson gson = new Gson();
 
     // TODO: Rename parameter arguments, choose names that match
@@ -92,21 +95,54 @@ public class ProductFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_product, container, false);
-        EditText edtPage = v.findViewById(R.id.edt_page);
-        Button prevBtn = v.findViewById(R.id.prev_btn);
-        Button nextBtn = v.findViewById(R.id.next_btn);
-        ListView listView = v.findViewById(R.id.product_list);
-//        ArrayAdapter adapter = new ArrayAdapter<String>(getContext(), R.layout.row_product_list, mobileArray);
+        edtPage = v.findViewById(R.id.edt_page);
+        prevBtn = v.findViewById(R.id.prev_btn);
+        nextBtn = v.findViewById(R.id.next_btn);
+        listView = v.findViewById(R.id.product_list);
+        goBtn = v.findViewById(R.id.go_btn);
+        String page = edtPage.getText().toString();
+        int pageNumber = Integer.valueOf(page);
+        getProductList(pageNumber - 1, 1);          //page di backend dimulai dari 0
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pageNow = Integer.valueOf(edtPage.getText().toString());
+                edtPage.setText((pageNow + 1) + "");
+                int pageNext = Integer.valueOf(edtPage.getText().toString());
+                getProductList(pageNext - 1, 1);
+            }
+        });
 
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pageNow = Integer.valueOf(edtPage.getText().toString());
+                edtPage.setText((pageNow - 1) + "");
+                int pagePrev = Integer.valueOf(edtPage.getText().toString());
+                getProductList(pagePrev - 1, 1);
+            }
+        });
+
+        goBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pageNow = Integer.valueOf(edtPage.getText().toString());
+                getProductList(pageNow - 1, 1);
+            }
+        });
+        return v;
+    }
+
+    public void getProductList(int page, int pageSize){
         Response.Listener<String> listener = new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
+                    productList.clear();
                     JSONArray object = new JSONArray(response);
                     Type productListType = new TypeToken<ArrayList<Product>>(){}.getType();
                     productList = gson.fromJson(response, productListType);
-//                    JsonArray object = new JsonParser().parse(response).getAsJsonArray();//new JSONArray(response);
-//                    productList = gson.fromJson(response, productList.getClass());
+                    List<String> productNameList = new ArrayList<>();
                     for(Product p : productList){
                         productNameList.add(p.name);
                     }
@@ -126,8 +162,6 @@ public class ProductFragment extends Fragment {
         };
 
         RequestQueue queue = Volley.newRequestQueue(getContext());
-        queue.add(RequestFactory.getPage("product", 0, 15, listener, errorListener));
-//        ArrayAdapter adapter = new ArrayAdapter<Product>(getContext(), R.layout.row_product_list, productList);
-        return v;
+        queue.add(RequestFactory.getPage("product", page, pageSize, listener, errorListener));
     }
 }
